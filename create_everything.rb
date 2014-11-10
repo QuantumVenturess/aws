4258f1ded9401fb1564a259eeda87a1615c2d2f3
Tommy Dang <quantumventuress@gmail.com>
2014-11-10 08:36:18 -0800

    finished test cloudformation

--- create_everything.rb
+++ create_everything.rb
@@ -1,3 +1,5 @@
+# http://docs.aws.amazon.com/sdkforruby/api/frames.html
+
 require "aws-sdk-v1"
 require "aws-sdk"
 
@@ -25,13 +27,16 @@ vpcs       = ec2_resource.vpcs
 peer_vpc   = vpcs.first
 cidr_block = "10.0.0.0/24"
 vpc        = ec2_resource.create_vpc(cidr_block: cidr_block)
+puts "VPC created"
 # Peering connection
 unless peer_vpc.nil?
   peering_connection = ec2_resource.create_vpc_peering_connection(
     peer_vpc_id: peer_vpc.id,
     vpc_id:      vpc.id
   )
+  puts "Peering Connection created"
   peering_connection.accept
+  puts "Peering Connection accepted"
   # or
   # peering_connection = vpc.requested_vpc_peering_connections.first
 end
@@ -49,6 +54,7 @@ security_group = ec2_resource.create_security_group(
   group_name:  "security-group-#{timestamp}",
   vpc_id:      vpc.id
 )
+puts "Security Group created"
 # Authorize
 # Inbound rules
 security_group.authorize_ingress(
@@ -58,6 +64,7 @@ security_group.authorize_ingress(
   # source_security_group_owner_id: "",
   to_port:        80
 )
+puts "Security Group inboud rule created"
 # Outbound rules
 security_group.authorize_egress(
   ip_permissions: [
@@ -73,12 +80,14 @@ security_group.authorize_egress(
     }
   ]
 )
+puts "Security Group outbound rule created"
 
 # Key Pair
 # Create a new key pair
 key_pair_name = "key-pair-#{timestamp}"
 key_pair      = ec2_resource.create_key_pair(key_name: key_pair_name)
-private_key   = key_pair.private_key # you can only call this once
+private_key   = key_pair.key_fingerprint # you can only call this once
+puts "Key Pair created"
 
 # EC2
 count = 2
@@ -96,7 +105,25 @@ instances     = ec2_resource.create_instances(
   security_group_ids: [security_group.id],
   subnet_id:          subnet.id
 )
+puts "EC2 Instances created"
 single_instance = instances.first
+
+# Internet Gateway
+internet_gateway = ec2_resource.create_internet_gateway
+puts "Internet Gateway created"
+# Attach internet gateway to vpc
+ec2_client.attach_internet_gateway(
+  internet_gateway_id: internet_gateway.id,
+  vpc_id:              vpc.id
+)
+puts "Internet Gateway attached"
+
+# Waiting
+puts "Waiting for EC2 Instance to be running..."
+single_instance.wait_until_running do |w|
+  w.interval     = 10
+  w.max_attempts = 100
+end
 
 # ELB Client
 elb_client = Aws::ElasticLoadBalancing::Client.new(
@@ -121,11 +148,13 @@ elb = elb_client.create_load_balancer(
   security_groups:    [security_group.id],
   subnets:            [subnet.id]
 )
+puts "Elastic Load Balancer created"
 # Add instance to load balancer
 elb_client.register_instances_with_load_balancer(
   load_balancer_name: load_balancer_name,
   instances: instances.map { |i| { instance_id: i.id } }
 )
+puts "Elastic Load Balancer registered EC2 Instances"
 
 # Create AMI
 image = single_instance.create_image(
@@ -133,6 +162,7 @@ image = single_instance.create_image(
   description: "image for create_everything.rb",
   no_reboot:   false
 )
+puts "AMI created"
 
 # Auto Scaling
 # Client
@@ -163,9 +193,11 @@ auto_scaling_client.create_launch_configuration(
   launch_configuration_name: launch_configuration_name,
   # security_groups: [security_group.id]
 )
+puts "Launch Configuration created"
 # Auto Scaling Groups
-# If you use instance id here, you don't need to create a previous launch config
-# this method will create a launch configuration for you using the instance id
+# If you use instance id here, 
+# you don't need to create a previous launch config
+# This method will create a launch configuration for you using the instance id
 auto_scaling_client.create_auto_scaling_group(
   auto_scaling_group_name:   "group-#{timestamp}",
   default_cooldown:          500,
@@ -178,3 +210,6 @@ auto_scaling_client.create_auto_scaling_group(
   # launch_configuration_name: launch_configuration_name,
   vpc_zone_identifier: subnet.id
 )
+puts "Auto Scaling Group created"
+
+# CloudFormation
